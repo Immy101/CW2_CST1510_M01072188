e9106cf7fc235a2a8fb5f0f104b2cd195672fc17
mary <immaculatekampire4@gmail.com>
2025-11-07 20:34:44 +0400

    changed some functions and added to the verify func

--- auth.py
+++ auth.py
@@ -5,10 +5,34 @@ def hash_password(password):
     salt=bcrypt.gensalt(rounds=12)
     hash=bcrypt.hashpw(password_bytes, salt)
     return hash
-def register(username, password):
+def verify(username, password):
+    password_bytes=password.encode('utf-8')
     hash_password1=hash_password(password)
-    with open("users.txt", 'w') as f:
-        f.write(f"{username}, {hash_password1}")
+    result1=bcrypt.checkpw(password_bytes, hash_password1)
+    if result1:
+        print(f"{username} is verified.")
+    else:
+        return False
+    return True
+def login(username, password):
+    with open("users.txt", 'r') as f:
+        for line in f.readlines():
+            try:
+                user, hashp, roles=line.strip().split(',',2)
+                if user == username:
+                    return verify(username, password)
+                return False
+            except ValueError:
+                continue
+def register(username, password, role):
+    hash_password1=hash_password(password)
+    with open("users.txt", 'a') as f:
+        #use "a" for append because the "w" for write would write over everything and erase the already entered information
+        f.write(f"{username}, {hash_password1}, {role}\n")
+    print(f"{username}, {role} has registered.")
+    return True
 username=input("Enter your username: ")
 password=input("Enter your password: ")
-register(username, password)
+role=input("Enter your role(position): ")
+register(username, password, role)
+login(username, password)
