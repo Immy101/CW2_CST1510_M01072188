a895da665a17e9b45624ce1ab6c6e81e177869b5
mary <immaculatekampire4@gmail.com>
2025-11-17 11:14:39 +0400

    added some functions

--- auth.py
+++ auth.py
@@ -25,14 +25,53 @@ def login(username, password):
             except ValueError:
                 continue
 def register(username, password, role):
+    password_strength=pw_strength(password)
     hash_password1=hash_password(password)
     with open("users.txt", 'a') as f:
         #use "a" for append because the "w" for write would write over everything and erase the already entered information
         f.write(f"{username}, {hash_password1}, {role}\n")
     print(f"{username}, {role} has registered.")
     return True
-username=input("Enter your username: ")
-password=input("Enter your password: ")
-role=input("Enter your role(position): ")
-register(username, password, role)
-login(username, password)
+def pw_strength(password):
+    if len(password) <=0:
+        print("password too short. invalid.")
+    else:
+        specialchar=["<>,./?:;|\}{[]_-=+!@#$%^&*()"]
+        for x in password:
+            if x==password.isdigit():
+                pass
+            elif x==password.isalpha():
+                pass
+            elif x==password.isspace():
+                pass
+            elif x in specialchar:
+                pass
+            else:
+                return False
+        return True
+#Welcome/ display page  
+on=True
+while on:
+    print("*"*60)
+    print("Multi-domain Intelligence platform")
+    print("*"*60)
+    print("Welcome! Please choose from the list of options below.")
+    print("*"*60)
+    print("1.LOGIN")
+    print("2.REGISTER")
+    print("*"*60)
+    option=input("Enter your choice: ")
+    if option == '1':
+        username=input("Enter your username: ")
+        password=input("Enter your password: ")
+        role=input("Enter your role(position): ")
+        login(username, password)
+        on= False
+    elif option == '2':
+        username=input("Enter your username: ")
+        password=input("Enter your password: ")
+        role=input("Enter your role(position): ")
+        register(username, password, role)
+        on= False
+    else:
+        print("invalid entry")
